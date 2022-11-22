a2d1506519ca6b60fd3fc62f22256c1c6dbe2d5f
Cary <CaryZhengMy@gmail.com>
2022-11-22 17:38:46 +0800

    Init project

---
+++ MyARKitDemo/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  MyARKitDemo
+//
+//  Created by Cary on 2022/11/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
--- MyARKitDemo/ViewController.swift
+++ MyARKitDemo/ViewController.swift
@@ -12,8 +12,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        let button = UIButton()
+        button.setTitle("启动AR", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.sizeToFit()
+        
+        button.frame.origin = CGPoint(x: 100, y: 100)
+        
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(onButtonClicked), for: .touchUpInside)
     }
 
+    @objc func onButtonClicked() {
+        print("onButtonClicked")
+        
+        self.navigationController?.pushViewController(SecondViewController(), animated: true)
+    }
 
 }
 
