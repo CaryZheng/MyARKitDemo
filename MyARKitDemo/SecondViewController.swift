3524a0fa8253a6dc1ac9abeca6772bf650893f80
Cary <CaryZhengMy@gmail.com>
2022-11-22 18:05:56 +0800

    Add AR code

--- MyARKitDemo/SecondViewController.swift
+++ MyARKitDemo/SecondViewController.swift
@@ -6,12 +6,43 @@
 //
 
 import UIKit
+import ARKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, ARSCNViewDelegate {
+    
+    var mSceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
+        
+        mSceneView = ARSCNView(frame: self.view.bounds)
+        mSceneView.delegate = self
+        self.view.addSubview(mSceneView)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("AR Resources 资源文件不存在!")
+        }
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+        mSceneView.session.run(configuration)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else { return }
+        
+        let referenceImage = imageAnchor.referenceImage
+        let plane = SCNPlane(width: referenceImage.physicalSize.width, height: referenceImage.physicalSize.height)
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        node.addChildNode(planeNode)
     }
 }
